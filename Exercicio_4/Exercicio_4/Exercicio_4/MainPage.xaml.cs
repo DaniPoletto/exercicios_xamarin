182b201bc3c424483ea79354052a2322ee14a854
daniela6tres <daniela@6tres.com.br>
2020-05-10 17:27:38 -0300

    Exercicio 4 - mes de baixa e alta temporada

---
+++ Exercicio_4/Exercicio_4/Exercicio_4/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Exercicio_4
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        int mes = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_temporada_Clicked(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txt_mes.Text, out mes))
+            {
+                DisplayAlert("Atenção", "Digite corretamente o mes", "Fechar");
+            }else if (mes<1 || mes>12)
+            {
+                DisplayAlert("Atenção", "Digite um mes entre 1 e 12.", "Fechar");
+            }
+            else
+            {
+                if (mes==1 || mes==2 || mes==6 || mes==7 || mes==12 )
+                {
+                    txt_resultado.Text = $"O mês {mes} é de alta temporada";
+                }
+                else
+                {
+                    txt_resultado.Text = $"O mês {mes} é de baixa temporada";
+                }
+            }
+        }
+    }
+}
